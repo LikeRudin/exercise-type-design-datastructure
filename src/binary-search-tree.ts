d163d5ae73159f1fb9c10861a70f95e29567159e
likeRudin <likeRudin@gmail.com>
2023-08-19 17:05:05 +0900

    add class NodeForBinarySearchTree

--- src/binary-search-tree.ts
+++ src/binary-search-tree.ts
@@ -1,5 +1,16 @@
 import { NodeForBinaryTree, BinaryTree } from "./type";
 
+class NodeForBinarySearchTree<T> implements NodeForBinaryTree<T>{
+    val: T;
+    left: NodeForBinaryTree<T> | null;
+    right: NodeForBinaryTree<T> | null;
+    constructor(val:T, left=null,right=null){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+} 
+
 class BinarySearchTree<T> implements BinaryTree<T> {
     root: NodeForBinaryTree<T> | null;
     constructor(){
