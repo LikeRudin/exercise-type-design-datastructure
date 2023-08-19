f4c6de605a9d53d871675e3bc24c8e02967d711c
likeRudin <likeRudin@gmail.com>
2023-08-19 17:53:37 +0900

    add insert method for BinarySearchTree Class

--- src/binary-search-tree.ts
+++ src/binary-search-tree.ts
@@ -2,8 +2,8 @@ import { NodeForBinaryTree, BinaryTree } from "./type";
 
 class NodeForBinarySearchTree<T> implements NodeForBinaryTree<T>{
     val: T;
-    left: NodeForBinaryTree<T> | null;
-    right: NodeForBinaryTree<T> | null;
+    left: NodeForBinarySearchTree<T> | null;
+    right: NodeForBinarySearchTree<T> | null;
     constructor(val:T, left=null,right=null){
         this.val = val;
         this.left = left;
@@ -12,10 +12,57 @@ class NodeForBinarySearchTree<T> implements NodeForBinaryTree<T>{
 } 
 
 class BinarySearchTree<T> implements BinaryTree<T> {
-    root: NodeForBinaryTree<T> | null;
+    root: NodeForBinarySearchTree<T> | null;
     constructor(){
         this.root = null;
     }
-    insert(){}
+    insert(val: T){
+        try{
+        const newNode = new NodeForBinarySearchTree(val);
+        if (this.root === null){
+            this.root = newNode;
+            return this
+        }
+
+        let nodeForComparison: NodeForBinarySearchTree<T> | null = this.root;
+        while (true){
+            let parent;
+            if (nodeForComparison!.val > newNode.val){
+                parent = nodeForComparison;
+                nodeForComparison = nodeForComparison!.left;
+            } else if (nodeForComparison!.val < newNode.val) {
+                parent = nodeForComparison;
+                nodeForComparison = nodeForComparison!.right;
+            } else {
+                throw new Error(`some node has already this value`)
+            }
+            if (nodeForComparison === null){
+                nodeForComparison = parent;
+                break;
+            }
+        }
+
+        if(nodeForComparison.val > newNode.val){
+            nodeForComparison.left = newNode;
+        }
+        else {
+            nodeForComparison.right = newNode;
+        }
+        return this
+
+        } catch (e) {
+            console.log(e)
+            return this;
+        }}
     search(){}
 }
+
+
+const myFirstTree = new BinarySearchTree<number>();
+
+console.log(myFirstTree.insert(3));
+console.log(myFirstTree.insert(5));
+console.log(myFirstTree.insert(2));
+console.log(myFirstTree);
+console.log(myFirstTree.insert(3));
+console.log(myFirstTree.insert(7));
